ea57b43f105b71e2bc54da0bbedfa638f94811b8
Hassanabdelqader <terawihassan@gmail.com>
2022-07-18 19:11:08 +0300

    add Some Chart

--- statistics.js
+++ statistics.js
@@ -1,4 +1,4 @@
-
+//const { Chart } = require("chart.js");
 
 
 let divresult=document.getElementById('tablediv');
@@ -11,11 +11,105 @@ tbody.setAttribute('id' , 'resultbody2');
 
 let arrFood = getFromLocal();
 
+let FruitandV = 0;
+let Starchy = 0;
+let Dairy = 0;
+let Protein = 0;
+let Fat = 0;
+
+const label = [];
+const data = [];
+const foodName = [];
+const foodPrice = [];
+
+function fillcounter(arr) {
+    arr.forEach(obj => {
+       foodName.push(obj.foodName);
+       foodPrice.push(obj.price);
+        switch (obj.type) {
+            case 'Starchy food':
+                ++Starchy;
+              break;
+            case 'Protein':
+                ++Protein;
+              break;
+            case 'Fruit and vegetables':
+                FruitandV++;
+              break;
+            case 'Fat':
+                Fat++;
+              break;
+            case 'Dairy':
+                Dairy++;
+              break;
+          }
+
+    });
+
+if (FruitandV) {
+    label.push('Fruit and vegetables');
+    data.push(FruitandV);
+}
+if (Starchy) {
+    label.push('Starchy food');
+    data.push(Starchy);
+}
+if (Dairy) {
+    label.push('Dairy');
+    data.push(Dairy);
+}
+if (Protein) {
+    label.push('Protein');
+    data.push(Protein);
+}
+if (Fat) {
+    label.push('Fat');
+    data.push(Fat);
+}
+
+
+}
+
+
+
+fillcounter(arrFood);
+
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels:label ,
+
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: data,
+                    backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(150, 155, 86)',
+                    'rgb(200, 50, 10)',
+                    'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 30
+                }]
+        
+        }, options: {
+            responsive: false,
+              onClick: function(evt, element) {
+               // alert('test');
+              }
+        }
+    });    
+
+    
+
+
 
 
 function showData(value){
 
-    console.log(value.foodName);
+   // console.log(value.foodName);
 
     if (value == undefined) {
         return;
@@ -150,4 +244,38 @@ for (let index = 0; index < arrFood.length; index++) {
 
 
 
- 
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+const labels =foodName;
+const data2 = {
+  labels: labels,
+  datasets: [{
+    label: 'Food Prices',
+    data: foodPrice,
+    backgroundColor: [
+      'blue'
+    ],
+    borderColor: [
+      'blue'
+    ],
+    borderWidth: 1
+  }]
+};
+const config = {
+    type: 'bar',
+    data: data2,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                steps : 10,
+                stepValue : 10 ,
+                max: 40,
+                ticks:{
+                    stepSize : 10 
+                } 
+                }
+        }
+    },
+  };
+
+  const myChar2 = new Chart(ctx2,config);
